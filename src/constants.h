dcce8859a6ea9a1a87f2eafe1f796b1fc83905ea
Scott Henderson <henders999@gmail.com>
2019-03-11 13:16:51 -0700

    Split PREFERRED_BUFFER into a buffer for vehicles in front and a separate constant for vehicles in other lanes. Also reformatted & added more comments.

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,27 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-constexpr auto MAX_S                   = 6945.554;  // The max s value before wrapping around the track back to 0.
-constexpr auto LANE_COUNT              = 3;         // Three lanes in each direction.
-constexpr auto LANE_WIDTH              = 4.0;       // Lane width in meters.
-                                                    // Half of the lane width in meters - make center lane calculation easier.
-constexpr auto HALF_LANE_WIDTH         = LANE_WIDTH / 2.0;
-constexpr auto PREVIOUS_POINTS_TO_KEEP = 10;        // Number of points to keep from previous path.
-constexpr auto SPEED_LIMIT             = 22.25;     // Max velocity in meters/second.
-                                                    // Speed limit == 50 mph ~= 22.353 m/s
-constexpr auto MAX_ACCELERATION        = 2.0;       // Max acceleration at each time step - can be negative for braking.
-constexpr auto PREFERRED_BUFFER        = 6.0;       // Preferred buffer between the ego vehicle and the vehicle in front.
+// Constants related to the track.
+constexpr auto MAX_S                        = 6945.554; // The max s value before wrapping around the track back to 0.
+constexpr auto LANE_COUNT                   = 3;        // Three lanes in each direction.
+constexpr auto LANE_WIDTH                   = 4.0;      // Lane width in meters.
+                                                        // Half of the lane width in meters.
+constexpr auto HALF_LANE_WIDTH              = LANE_WIDTH / 2.0;
+
+// Speed limit in meters/second (50 mph ~= 22.353 m/s).
+constexpr auto SPEED_LIMIT                  = 22.25;
+
+// Max acceleration in meters/second/second (0.1 m/s/s ~ 0.326 ft/s/s). 
+constexpr auto MAX_ACCELERATION             = 0.1;
+
+// Preferred buffer distance (meters) between the ego vehicle and other vehicles.
+constexpr auto PREFERRED_BUFFER_FRONT       = 6.0;      // Distance to a vehicle in front.
+constexpr auto PREFERRED_BUFFER_LANE_CHANGE = 30.0;     // Distance to any vehicle in the target lane for lane changes.
+
+// Number of points to keep from the previous path for each iteration.
+// Use a larger number to maintain a smoother trajectory.
+// Use a smaller value to allow the ego vehicle to repond more quickly
+// to changing conditions.
+constexpr auto PREVIOUS_POINTS_TO_KEEP      = 10;
 
 #endif CONSTANTS_H
