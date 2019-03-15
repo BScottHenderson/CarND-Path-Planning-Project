d1d83e377750c5021cbded8d58d3744664f7276e
Scott Henderson <scott.henderson@weyerhaeuser.com>
2019-03-15 10:51:58 -0700

    Added constants for setting prediction horizon based on current ego velocity.

--- src/constants.h
+++ src/constants.h
@@ -10,14 +10,22 @@ constexpr auto LANE_WIDTH                   = 4.0;      // Lane width in meters.
                                                         // Half of the lane width in meters.
 constexpr auto HALF_LANE_WIDTH              = LANE_WIDTH / 2.0;
 
+// Interval at which vehicle updates occur.
+constexpr auto UPDATE_INTERVAL              = 0.02;     // Update interval in seconds (20ms == 0.02s).
+
 // Speed limit in meters/second (50 mph ~= 22.353 m/s).
 constexpr auto SPEED_LIMIT                  = 22.25;
 
 // Max acceleration in meters/second/second.
-constexpr auto MAX_ACCELERATION             = 0.075;
+constexpr auto MAX_ACCELERATION             = 0.1;
 
-// Number of time steps for predictions.
-constexpr auto PREDICTION_HORIZON           = 5;
+// Number of time steps for the prediction horizon.  A lane change takes roughly 3s to
+// complete at lower speeds. Use this value to determine the max prediction horizon.
+constexpr auto MIN_PREDICTION_HORIZON       = (int) (0.1 / UPDATE_INTERVAL);    // 0.1 s
+constexpr auto MAX_PREDICTION_HORIZON       = (int) (3.0 / UPDATE_INTERVAL);    // 3.0 s
+
+// Slope of a line representing a linear relationship between prediction horizon and speed.
+constexpr auto PREDICTION_HORIZON_SLOPE     = (MAX_PREDICTION_HORIZON - MIN_PREDICTION_HORIZON) / SPEED_LIMIT;
 
 // Preferred buffer distance (meters) between the ego vehicle and other vehicles.
 constexpr auto PREFERRED_BUFFER_FRONT       = 6.0;      // Distance to a vehicle in front.
@@ -29,9 +37,6 @@ constexpr auto PATH_STEP                    = 30.0;
 // Number of waypoints to add to the list of path points before using
 // spline to smooth the trajectory.
 constexpr auto WAYPOINTS_TO_ADD             = 3;
-
-// Interval at which vehicle updates occur.
-constexpr auto UPDATE_INTERVAL              = 0.02;     // Update interval in seconds (20ms == 0.02s).
 
 // Number of points in a path for 1 second.
 // With a 0.02s update interval this value should be 50.
