7247285be57b3a3c767670475a4a0f0429a24e10
Scott Henderson <henders999@gmail.com>
2019-03-25 14:22:11 -0700

    Added debug.h for global objects & precompiler constants.

---
+++ src/debug.h
@@ -0,0 +1,12 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define USE_VEHICLE_CLASS_PLANNING  0
+
+#define WRITE_PREDICTIONS           0
+
+#include "log_writer.h"
+
+extern LogWriter    log_file;
+
+#endif DEBUG_H
