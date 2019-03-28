850cb62227ea4afc0640ea082ed51cc220935341
BScottHenderson <henders999@gmail.com>
2019-03-28 11:49:02 -0700

    Added experimental (and not working) conditionally-compiled code to use JMT for smoothing instead of spline.

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -9,7 +9,16 @@
 #include "JMT.h"
 #include "debug.h"
 
+// Use generated predictions (e.g., from Vehicle::generate_predictions())?
+#define USE_PREDICTIONS     1
+
+// Stay in the center (starting) lane? Otherwise attempt to move to the rightmost
+// lane as traffic allows.
 #define STAY_IN_CENTER_LANE 1
+
+// Path smoothing option - spline vs. jerk minimizing trajectory
+#define SMOOTH_SPLINE       1
+#define SMOOTH_JMT          0
 
 
 void StraightLineTrajectory(double car_x, double car_y, double car_yaw,
@@ -59,46 +68,25 @@ void UpdateEgo(
     bool    car_ahead = false;
     bool    car_left  = false;
     bool    car_right = false;
-    //for (auto pred : predictions) {
-    //    auto    car = pred.second[0];
-    //    // Adjust the other vehicle 's' position by adding 'prev_path_size' steps.
-    //    // We know that each step is 20ms (0.02s) and that the other vehicle
-    //    // is moving at velocity 'car.v'.
-    //    //double  t = prev_path_size * UPDATE_INTERVAL;
-    //    //double  check_car_s = car.s + car.v * t;
-
-    //    double  t = (prev_path_size - 1) * UPDATE_INTERVAL;
-    //    double  check_car_s =
-    //        (pred.second.size() > prev_path_size)
-    //        ? pred.second[prev_path_size - 1].s
-    //        : car.s + car.v * t;
-
-    //    // Identify whether other vehicle is ahead, to the left, or to the right.
-    //    switch (car.lane - ego.lane) {
-    //    case 0:     // Car in our lane.
-    //        // If the car is ahead of us and within the buffer distance, set the flag.
-    //        if (ego.s < check_car_s &&
-    //                    check_car_s < (ego.s + PREFERRED_BUFFER))
-    //            car_ahead = true;
-    //        break;
-    //    case -1:    // Car to the left.
-    //        // If car is within the buffer distance of our location, set the flag.
-    //        if ((ego.s - PREFERRED_BUFFER) < check_car_s &&
-    //                                         check_car_s < (ego.s + PREFERRED_BUFFER))
-    //            car_left = true;
-    //        break;
-    //    case 1:     // Car to the right.
-    //        // If car is within the buffer distance of our location, set the flag.
-    //        if ((ego.s - PREFERRED_BUFFER) < check_car_s &&
-    //                                         check_car_s < (ego.s + PREFERRED_BUFFER))
-    //            car_right = true;
-    //        break;
-    //    }
-    //}
-
     for (auto pred : predictions) {
+#if USE_PREDICTIONS
         for (auto car : pred.second) {
-            // Identify whether other vehicle is ahead, to the left, or to the right.
+#else
+        auto    car = pred.second[0];
+        // Adjust the other vehicle 's' position by adding 'prev_path_size' steps.
+        // We know that each step is 20ms (0.02s) and that the other vehicle
+        // is moving at velocity 'car.v'.
+        //double  t = prev_path_size * UPDATE_INTERVAL;
+        //double  check_car_s = car.s + car.v * t;
+
+        double  t = (prev_path_size - 1) * UPDATE_INTERVAL;
+        double  check_car_s =
+            (pred.second.size() > prev_path_size)
+            ? pred.second[prev_path_size - 1].s
+            : car.s + car.v * t;
+        {
+#endif // USE_PREDICTIONS
+            // Identify whether the other vehicle is ahead, to the left, or to the right.
             switch (car.lane - ego.lane) {
             case 0:     // Car in our lane.
                 // If the car is ahead of us and within the buffer distance, set the flag.
@@ -161,7 +149,7 @@ void UpdateEgo(
         // since the vehicle does not start in the rightmost lane.
         if (ego.v > 0.0 && ego.lane < MAX_LANE && !car_right) {
             ego.lane++;
-            log_file.write("not in right lane - change lane right");
+            log_file.write("Not in rightmost lane - change lane right.");
         }
 #endif
 
@@ -175,10 +163,18 @@ void UpdateEgo(
 
 void PathPlannerTrajectory(
     Vehicle& ego,
-    std::vector<double>& map_waypoints_x, std::vector<double>& map_waypoints_y, std::vector<double>& map_waypoints_s,
-    std::vector<double>  previous_path_x, std::vector<double>  previous_path_y,
-    std::vector<double>& next_x_vals,     std::vector<double>& next_y_vals) {
-    // Generate a smooth path for the ego vehicle.
+    std::vector<double>& map_waypoints_x,  std::vector<double>& map_waypoints_y,  std::vector<double>& map_waypoints_s,
+    std::vector<double>& map_waypoints_dx, std::vector<double>& map_waypoints_dy,
+    std::vector<double>  previous_path_x,  std::vector<double>  previous_path_y,
+    std::vector<double>& next_x_vals,      std::vector<double>& next_y_vals) {
+    /*
+        Generate a smooth path for the ego vehicle.
+    */
+
+    //
+    // Add points from the previous path. The goal of this step is to ensure a smooth
+    // transition between the previous path and the path we're generating here.
+    //
 
     int     prev_path_size = std::min(PREVIOUS_POINTS_TO_KEEP, (int) previous_path_x.size());
 
@@ -195,6 +191,10 @@ void PathPlannerTrajectory(
     }
 
 
+    //
+    // Find the starting point and angle 
+    //
+
     // Calculate the duration for each path (this should always be 1.0s).
     double  t = PATH_LENGTH * UPDATE_INTERVAL;
 
@@ -210,10 +210,6 @@ void PathPlannerTrajectory(
     log_file.write(ss);
     ss << "  (lane, lane_d) : (" << ego.lane << ", " << ego_d << ")";
     log_file.write(ss);
-
-    // Collect points for the path we want to follow.
-    vector<double>  path_x;
-    vector<double>  path_y;
 
     // Starting point & angle.
     double  start_x;
@@ -235,8 +231,9 @@ void PathPlannerTrajectory(
         //  x' = x + d cos(theta)
         //  y' = y + d sin(theta)
         // We're backing up by one meter so d == 1 and we subtract instead of add.
-        //prev_x = start_x - cos(car_yaw);
-        //prev_y = start_y - sin(car_yaw);
+        //double  theta = deg2rad(car_yaw);
+        //prev_x = start_x - cos(theta);
+        //prev_y = start_y - sin(theta);
 
         // Rather than using the bicycle model to calculate a hypothetical previous
         // car position, just backup by one path step interval in the same lane -
@@ -258,6 +255,18 @@ void PathPlannerTrajectory(
         prev_x = previous_path_x[prev_path_size-2];
         prev_y = previous_path_y[prev_path_size-2];
     }
+
+#if SMOOTH_SPLINE & SMOOTH_JMT
+
+    throw "Invalid smoothing option in trajectory.cpp - choose a single option.";
+
+#elif SMOOTH_SPLINE
+
+    // Collect points for the path we want to follow.
+    vector<double>  path_x;
+    vector<double>  path_y;
+
+    // Add the starting point and the previous point to our list.
     path_x.push_back(prev_x);  path_y.push_back(prev_y);
     path_x.push_back(start_x); path_y.push_back(start_y);
 
@@ -332,4 +341,71 @@ void PathPlannerTrajectory(
         next_x_vals.push_back(new_x);
         next_y_vals.push_back(new_y);
     }
+
+#elif SMOOTH_JMT
+
+    double  cur_x = start_x;
+    double  cur_y = start_y;
+    int     nextWaypoint = NextWaypoint(cur_x, cur_y, angle, map_waypoints_x, map_waypoints_y);
+
+    double  dist_inc = 0.5; // ~56 mph
+
+    for (int i = 0; i < PATH_LENGTH - prev_path_size; ++i, ++nextWaypoint) {
+        // Location of the next waypoint.
+        double  next_x = map_waypoints_x[nextWaypoint] + map_waypoints_dx[nextWaypoint] * ego_d;
+        double  next_y = map_waypoints_y[nextWaypoint] + map_waypoints_dy[nextWaypoint] * ego_d;
+
+        // Angle from (cur_x, cur_y) to (next_x, next_y).
+        angle = atan2(next_y - cur_y, next_x - cur_x);
+
+        // Distance to the next waypoint.
+        double  dist = distance(cur_x, cur_y, next_x, next_y);
+        if (dist <= 0.000001) { // We're essentially at the next waypoint, so move on.
+            --i;    // We didn't actually add a point in this iteration.
+            continue;
+        }
+
+        // How long are we going to take to get to the next waypoint?
+        double  T = dist / dist_inc;
+
+        // Translate (cur_x, cur_y) and (next_x, next_y) to Frenet s coordinates.
+        // Keep the car speed constant with 0.0 acceleration.
+        vector<double>  sd;
+             // cur_x, cur_y
+        sd = getFrenet(cur_x, cur_y, angle, map_waypoints_x, map_waypoints_y);
+        double  s1, s1_dot, s1_ddot;
+        s1 = sd[0]; s1_dot = ego.v; s1_ddot = 0.0;
+        sd = getFrenet(next_x, next_y, angle, map_waypoints_x, map_waypoints_y);
+            // next_x, next_y
+        double  s2, s2_dot, s2_ddot;
+        s2 = sd[0], s2_dot = ego.v; s2_ddot = 0.0;
+        double  next_d = sd[1];
+
+        // Calculate coefficients for JMT between (cur_x, cur_y) and (next_x, next_y).
+        vector<double>  start = {s1, s1_dot, s1_ddot};
+        vector<double>  end   = {s2, s2_dot, s2_ddot};
+        vector<double>  a = JMT(start, end, T);
+
+        // Use the JMT to add points between (cur_x, cur_y) and (next_x, next_y).
+        for (int t = 0; t < T && i < 50 - prev_path_size; ++t, ++i) {
+            // s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+            double  next_s = 0.0;
+            for (int k = 0; k < 6; ++k)
+                next_s += a[k] * pow(t, k);
+
+            // Convert (s, d) to (x, y).
+            vector<double>  xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            cur_x = xy[0] + map_waypoints_dx[nextWaypoint] * ego_d;
+            cur_y = xy[1] + map_waypoints_dy[nextWaypoint] * ego_d;
+
+            next_x_vals.push_back(cur_x);
+            next_y_vals.push_back(cur_y);
+        }
+    }
+
+#else
+
+    throw "Smoothing option not set in trajectory.cpp.";
+
+#endif // SMOOTHING
 }
